83f4b6890b55a49441706ff072a93460d4be901b
Pavelrl8 <pavelito200@gmail.com>
2016-10-11 12:12:22 -0500

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("mysql.hostinger.es", "u679669756_pavel", "12345pavel", "u679669756_data");
     
     $name = $_POST["name"];
     $age = $_POST["age"];
